a58a1f841cc8284ba576f98d4a760768f9a1722e
inisebuahnama <nrafaelwr@gmail.com>
2026-02-06 16:23:00 +0700

    Update Google OAuth callback URL for Heroku

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ module.exports = (passport) => {
             {
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: process.env.GOOGLE_CALLBACK_URL || "/api/auth/google/callback",
+                callbackURL: process.env.GOOGLE_CALLBACK_URL || "https://cuan-shop-830d14389646.herokuapp.com/api/google/callback",
             },
             async (accessToken, refreshToken, profile, done) => {
                 try {
