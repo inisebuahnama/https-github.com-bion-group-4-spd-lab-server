224722ceaa88499ce9ed4e0c40658d50fbf77be6
inisebuahnama <nrafaelwr@gmail.com>
2026-02-06 16:25:10 +0700

    Remove legacy onrender CORS origin

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ connectDB();
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173", "https://cuan-shop-client-app.onrender.com"],
+  origin: ["http://localhost:5173"],
   credentials: true
 }));
 
