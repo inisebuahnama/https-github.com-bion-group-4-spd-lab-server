2c8055db84383ba23ad5385512aceb975bc635a0
inisebuahnama <nrafaelwr@gmail.com>
2026-02-06 16:16:57 +0700

    Fix app crash: remove duplicate PORT variable

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ app.get("/", (req, res) => {
   res.send("testestesaja");
 });
 // Run Server
-const PORT = process.env.PORT || 8000;
+// const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server is running on ", PORT);
 });
